fe5a8e32129401e8d7705307bbc4ce717b2bec87
jspark6815 <jspark6815@daum.net>
2025-12-08 03:24:56 +0900

    fix: Express.User 인터페이스 확장 수정
    - Passport.js의 Express.User 인터페이스를 JwtPayload로 확장
    - req.user?.sub 접근 시 타입 오류 해결

--- apps/backend/src/common/interfaces/express.d.ts
+++ apps/backend/src/common/interfaces/express.d.ts
@@ -5,13 +5,14 @@ import { JwtPayload } from './jwt-payload.interface';
  * 
  * AuthGuard에서 JWT 검증 후 req.user에 사용자 정보를 추가할 때
  * 타입 안전성을 보장하기 위한 전역 타입 선언
+ * 
+ * Note: Passport.js가 Express.User를 사용하므로, 
+ * User 인터페이스를 JwtPayload로 확장해야 함
  */
 declare global {
   namespace Express {
-    interface Request {
-      /** JWT 페이로드 (인증된 사용자 정보) */
-      user?: JwtPayload;
-    }
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface User extends JwtPayload {}
   }
 }
 
